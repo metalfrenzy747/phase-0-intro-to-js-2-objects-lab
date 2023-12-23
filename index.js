f0f0f5a8b85763fc91363b82d19952cee809fa0b
metalfrenzy747 <allenjeremy747@gmail.com>
2023-12-23 11:27:41 -0500

    commited

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Write your solution in this file!
+const employee= {
+    name: "Sam",
+    streetAddress: "11 Broadway",
+};
+function updateEmployeeWithKeyAndValue(employee, key, value){
+    const newEmployee= {...employee};
+    newEmployee[key]= value;
+    return newEmployee;
+}
+function destructivelyUpdateObject(employee, key, value)
+{
+    employee[key]=value;
+    return employee
+}
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value){
+    const newKeyAndValue= destructivelyUpdateObject(employee, key, value)
+    return newKeyAndValue;   
+}
+function deleteFromEmployeeByKey(employee, key){
+    const deleteKey= {...employee[key]};
+    delete deleteKey[key];
+    return deleteKey;
+}
+function destructivelyDeleteFromEmployeeByKey(employee, key){
+    delete employee[key];
+    return employee;
+}
